7c260cbb80e0c0716acbfa02bc1764fb00f4fa97
Nathan <nathan.forman@gmail.com>
2021-11-01 12:44:16 -0700

    Finished accounts model

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,34 @@
+const db = require('../../data/db-config.js');
+
 const getAll = () => {
   // DO YOUR MAGIC
+  const result = await db('accounts')
+  return result
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  const result = await db('accounts').where('id', id)
+  return result
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert(account)
+  const newAccount = await getById(id)
+  return newAccount
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  await db('accounts').update(account).where('id', id)
+  return getById(id)
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  const result = await db('accounts').del().where('id', id)
+  return result
 }
 
 module.exports = {
